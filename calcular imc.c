730040dc8a25416ec4af6b749b8704dba790eebf
jmoraesxx <113164840+jmoraesxx@users.noreply.github.com>
2022-09-09 04:52:54 -0300

    Add files via upload

---
+++ calcular imc.c
@@ -0,0 +1,31 @@
+//calcular imc
+
+#include <stdio.h>
+
+
+int main()
+{
+	float altura;
+	float peso;
+	float imc;
+	
+	printf("digite a sua altura: \n");
+	scanf("%f",&altura);
+	
+	printf("digite o sue peso: \n");
+	scanf("%f",&peso);
+	
+	imc = peso/(altura*altura);
+	
+	if(imc <= 18.4){
+		printf("abaixo do peso \n");
+	}else if(imc >= 18.5, imc <= 24.9){
+		printf("imc peso normal \n");
+    }else if(imc >=25 , imc <= 29.9){
+    	printf("imc sobre peso \n");
+	}else if(imc >= 30 , imc <= 39.9){
+		printf("obesidade \n");
+	}else{
+		printf("obesidade grave \n");
+	}
+}
