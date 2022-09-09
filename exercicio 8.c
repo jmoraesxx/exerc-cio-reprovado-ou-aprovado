ffc9ac9916533c49d519fa66c83688869f3b96b5
joao moraes <113164840+jmoraesxx@users.noreply.github.com>
2022-09-09 06:23:45 -0300

    Add files via upload

---
+++ exercicio 8.c
@@ -0,0 +1,24 @@
+//ler o ano do nascimento e o ano atual, calcular e imprimir a idade da pessoas
+
+#include<stdio.h>
+#include <locale.h> 
+
+int main()
+{
+setlocale (LC_ALL,"portuguese");
+
+int ano1, ano2, idade;
+
+printf("digite o ano que você vasceu... ");
+scanf("%d",&ano1);
+
+printf("digite o ano atual... ");
+scanf("%d",&ano2);
+
+idade = ano2 - ano1;
+
+printf("sua idade é %d", idade);
+
+return 0;
+
+}
