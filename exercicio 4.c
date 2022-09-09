cc91a3df70169a056de423971358d4b1470e2712
joao moraes <113164840+jmoraesxx@users.noreply.github.com>
2022-09-09 06:30:54 -0300

    Add files via upload

---
+++ exercicio 4.c
@@ -0,0 +1,20 @@
+//exercicio 4
+
+#include<stdio.h>
+
+int main(void)
+
+{
+
+int numero;
+
+printf("Digite um numero: ");
+scanf("%d",&numero);
+
+if(numero > 20){
+  printf("a metade desse numero e:  %d", numero / 2);
+}
+
+return 0;
+
+}
